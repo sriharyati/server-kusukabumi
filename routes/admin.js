2d4de8e63f8a3e62cd789149c61f0a32cb1e6e8f
sriharyati <sriharyati2599@gmail.com>
2021-06-17 23:21:36 +0700

    project backend

---
+++ routes/admin.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const adminController = require('../controllers/adminController');
+const { uploadSingle, uploadMultiple } = require('../middlewares/multer');
+
+router.get('/dashboard', adminController.viewDashboard);
+//end point category
+router.get('/category', adminController.viewCategory);
+router.post('/category', adminController.addCategory);
+router.put('/category', adminController.editCategory);
+router.delete('/category/:id', adminController.deleteCategory);
+//endpoint bank
+router.get('/bank', adminController.viewBank);
+router.post('/bank', uploadSingle, adminController.addBank);
+router.put('/bank', uploadSingle, adminController.editBank);
+router.delete('/bank/:id', adminController.deleteBank);
+//endpoint item
+router.get('/item', adminController.viewItem);
+router.post('/item', uploadMultiple, adminController.addItem);
+router.get('/item/show-image/:id', adminController.showImageItem);
+router.get('/item/:id', adminController.showEditItem);
+router.put('/item/:id', uploadMultiple, adminController.editItem);
+router.delete('/item/:id/delete', adminController.deleteItem);
+
+//endpoint detai item
+router.get('/item/show-detail-item/:itemId', adminController.viewDetaiItem);
+router.post('/item/add/show-detail-item', uploadSingle, adminController.addFeature);
+
+router.get('/booking',  adminController.viewBooking);
+
+module.exports = router;
